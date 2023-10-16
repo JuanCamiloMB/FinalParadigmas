f99c5843779afaeb79cd586b8e899896b481c4f2
JuanK <juancamilo.manjarres@gmail.com>
2023-10-16 11:43:28 -0500

    Fetch done

--- Frontend/src/pages/Products.jsx
+++ Frontend/src/pages/Products.jsx
@@ -1,5 +1,38 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 const Products = () => {
-    return <h1>Products</h1>;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = async () => {
+    const response = await fetch("http://localhost:3000/products");
+    setProducts(await response.json());
   };
-  
+
+  return (
+    <>
+      <h1>Products</h1>
+      <ul>
+        {products.map((product) => {
+          return (
+            <li key={product.id}>
+              <ul>
+                <li>{product.name}</li>
+                <li>{product.price}</li>
+                <li>{product.description}</li>
+                <li>{product.rating}</li>
+                <li>{product.stock}</li>
+              </ul>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
+
 export default Products;
